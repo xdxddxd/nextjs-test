13cf42c7dd626d8d9be3751edc5932b511d1d397
Derik Marco <38227342+xdxddxd@users.noreply.github.com>
2020-11-11 11:36:56 -0300

    insert contador

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
 export default function Home() {
-    return <div>Home</div>
+    return <Contador />
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionar() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+        <div>Home</div>
+        <button onClick={adicionar}>{contador}</button>
+        </div>
+    );
+
 }
